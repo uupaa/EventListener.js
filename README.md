e00204418489334f579e9a56db902f35fe027215
obara.takao <obara.takao@dena.jp>
2014-03-31 00:13:03 +0900

    first revision

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-EventListener.js
-================
 
-EventListener base class
+EventListener.js
+=========
+
+EventListener Class
+
+# Document
+
+https://github.com/uupaa/EventListener.js/wiki/EventListener
+
+# How to use
+
+```js
+<script src="lib/EventListener.js">
+<script>
+// for Browser
+console.log( new EventListener() );
+</script>
+```
+
+```js
+// for WebWorkers
+importScripts("lib/EventListener.js");
+
+console.log( new EventListener() );
+```
+
+```js
+// for Node.js
+var EventListener = require("lib/EventListener.js");
+
+console.log( new EventListener() );
+```
+
+# for Developers
+
+1. Install development dependency tools
+
+    ```sh
+    $ brew install closure-compiler
+    $ brew install node
+    $ npm install -g plato
+    ```
+
+2. Clone Repository and Install
+
+    ```sh
+    $ git clone git@github.com:uupaa/EventListener.js.git
+    $ cd EventListener.js
+    $ npm install
+    ```
+
+3. Build and Minify
+
+    `$ npm run build`
+
+4. Test
+
+    `$ npm run test`
+
+5. Lint
+
+    `$ npm run lint`
+
+
---
+++ test/test.js
@@ -0,0 +1,42 @@
+new Test().add([
+        testEventListener,
+    ]).run(function(err, test) {
+        if (1) {
+            err || test.worker(function(err, test) {
+                if (!err && typeof EventListener_ !== "undefined") {
+                    var name = Test.swap(EventListener, EventListener_);
+
+                    new Test(test).run(function(err, test) {
+                        Test.undo(name);
+                    });
+                }
+            });
+        }
+    });
+
+function testEventListener(next) {
+
+    var event = new EventListener();
+
+    event.types(["a", "b", "c"]);
+    event.add("a", function() { console.log("a"); });
+    event.add("a", function() { console.log("a"); });
+    event.add("b", function() { console.log("b"); });
+
+    var aEvents = event.get("a").length === 2;
+    var bEvents = event.get("b").length === 1;
+
+    event.clear();
+
+    var aEvents2 = event.get("a").length === 0;
+    var bEvents2 = event.get("b").length === 0;
+
+    if (aEvents && bEvents && aEvents2 && bEvents2) {
+        console.log("testEventListener ok");
+        next && next.pass();
+    } else {
+        console.error("testEventListener ng");
+        next && next.miss();
+    }
+}
+
